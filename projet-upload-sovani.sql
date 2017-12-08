974bac0f15a1f1dd1f17ff6b8901786f8908367a
Valentin <valentinlaidet1@gmail.com>
2017-12-08 12:36:33 +0100

    Ajout du sql

---
+++ projet-upload-sovani.sql
@@ -0,0 +1,47 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  ven. 08 déc. 2017 à 11:35
+-- Version du serveur :  5.7.19
+-- Version de PHP :  7.1.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `projet-upload-sovani`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `t_form`
+--
+
+DROP TABLE IF EXISTS `t_form`;
+CREATE TABLE IF NOT EXISTS `t_form` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `expediteur` varchar(50) NOT NULL,
+  `destinataire` varchar(50) NOT NULL,
+  `message` varchar(500) NOT NULL,
+  `link_upload` varchar(500) NOT NULL,
+  `link_files` varchar(500) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=53 DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
